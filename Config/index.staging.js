12cbc5b1be0be92305854d1bf9ac99b7a555fed1
UmaR Aamer <umaraamer@gmail.com>
2019-07-13 01:12:08 +0500

    Update index.staging.js

--- Config/index.staging.js
+++ Config/index.staging.js
@@ -1,4 +1,4 @@
 export const Config = {
-    API_URL: 'http://api.iparty.hypnosdev.com',
-    TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjIsInR5cGUiOiJ2ZW51ZSIsImlhdCI6MTU0MDQ3ODAzMCwiZXhwIjoxNTcyMDE0MDMwfQ.S-Yr_MH7Yp6jd13bKPiGnrNit5ZXD1lYhimmFK2eHz8'
+    API_URL: '',
+    TOKEN: ''
 }
